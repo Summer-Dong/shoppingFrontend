4377afdd60ee093c98f7a78b95485f376b7a36f3
Summer-Dong <D__yanan@163.com>
2016-07-19 16:10:39 +0800

    Fixed #5

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,36 @@
  * Controller of the shoppingFrontend
  */
 angular.module('shoppingFrontend')
-  	.controller('MainCtrl', function(itemService) {
+  	.controller('MainCtrl', function(itemService, rulesService) {
         var vm = this;
+        vm.itemInCart = []; /*itemInCart用于记录购物车中已有商品*/
         
         itemService.getItems()
             .then(function(result) {
                 vm.items = result;
             });
-    
+    	rulesService.getRules()
+            .then(function(result) {
+                vm.rules = result;
+            });
+
+
+        /*根据商品点击事件添加购物车中不存在的商品*/
+        vm.addToCart = function(item) {
+            var result = _.chain(vm.itemInCart)
+                .map(function(o) {
+                    return o.barcode;
+                })
+                .flatten()
+                .some(function(barcode) {
+                    return item.barcode === barcode;
+                })
+                .value();
+
+            if (!result) {
+                var addItem = angular.copy(item);
+                addItem.amount = 1;
+                vm.itemInCart.push(addItem);
+            }
+        };
 });
