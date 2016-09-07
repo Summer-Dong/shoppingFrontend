925e728ae65fdb1bb366f2ea93ca65e2cd097da4
Summer-Dong <D__yanan@163.com>
2016-09-07 16:35:11 +0800

    [11]add hasDiscountService

---
+++ app/scripts/service/hasDiscountService.js
@@ -0,0 +1,33 @@
+angular.module('shoppingFrontend')
+    .service('hasDiscountServ', function(rulesService) {
+        var self = this;
+        rulesService.getRules()
+            .then(function(result) {
+                self.rules = result;
+            });
+
+         self.hasDiscountOne = function(item) {
+            return _.chain(self.rules)
+                .dropRight(self.rules, 1)
+                .map(function(item) {
+                    return item.barcodes;
+                })
+                .flatten()
+                .some(function(barcode) {
+                    return item.barcode === barcode;
+                })
+                .value();
+        };
+        self.hasDiscountTwo = function(item) {
+            return _.chain(self.rules)
+                .drop(self.rules, 1)
+                .map(function(item) {
+                    return item.barcodes;
+                })
+                .flatten()
+                .some(function(barcode) {
+                    return item.barcode === barcode;
+                })
+                .value();
+        };
+    });
